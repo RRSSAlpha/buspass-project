4c3cfde45b6c8e369c45b71cafe22cb5cb12684b
RRSSAlpha <45935680+RRSSAlpha@users.noreply.github.com>
2019-01-16 17:56:51 +0530

    Add files via upload

---
+++ login page.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="login page.aspx.cs" Inherits="buspass.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <style type="text/css">
+        .auto-style1 {
+            width: 500px;
+            height: 300px;
+            border-style: solid;
+            border-width: 0px;
+        }
+    </style>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server" >
+    <%--<img src="bus.png" alt="white bus" width="100" height="100"> --%>
+   
+      
+    <table align="center" class="auto-style1">
+        <tr>
+            <td colspan="2"><asp:Image ID="Image1" runat="server" ImageUrl="C:\Users\supritha.kulkarni\Pictures\bus.png" Height="189px" Width="486px" />&nbsp;</td>
+            
+        </tr>
+        <tr>
+            <td>  <asp:Label ID="Label1" runat="server" Text="UserId"></asp:Label> &nbsp;</td>
+            <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>&nbsp;</td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>&nbsp;</td>
+            <td> <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"></asp:TextBox>&nbsp;</td>
+        </tr>
+        <tr>
+            <td> <asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" />&nbsp;</td>
+            <td> <asp:Button ID="Button2" runat="server" Text="Reset" OnClick="Button2_Click" />&nbsp;</td>
+        </tr>
+    </table>
+    
+   
+    <br />
+    <asp:Label ID="lblmsg" runat="server"></asp:Label>
+    
+   
+    <br />
+   
+ 
+     
+</asp:Content>
---
+++ login page.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace buspass
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //string EmpId = TextBox1.Text;
+            //string Pass = TextBox2.Text;
+            string connection = "server=WSLKCMP5F-582; user id=sa; password=slk@SOFT; database=bp";
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                string query = "select * from bp_table where EmpId='" + TextBox1.Text + "'and Password='" + TextBox2.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader Sdr = cmd.ExecuteReader();
+                if (Sdr.Read())
+                 {
+                     int Role = (int)Sdr["RoleId"];
+                     if (Role == 1111)
+                        Response.Redirect("admin.aspx",true);
+                     else if (Role == 0000)
+                        Response.Redirect("user.aspx",true);
+                     else
+                        lblmsg.Text = "record not found";
+                 }
+               else
+                      lblmsg.Text = "invalid user";
+            }
+
+           // conn.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+        }
+    }
+}
